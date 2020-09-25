5cfec7d798abbad0102f1d1b251fac065b989b9b
Justin Passchier <juspasschier@gmail.com>
2020-09-25 13:35:53 +0200

    Finished opdr 1 of loops

--- loops.php
+++ loops.php
@@ -1,12 +1,36 @@
 <?php
-// Opdracht 1
-$ster="";
-for ($a= 0; $a<=3; $a=$a+1){
-    $extraster="*";
-    $ster=$ster.$extraster;
-    print ("$ster\n");
-}
-for ($i= 3; $a >=3; $a=$a-1){
-//    substr($ster, 0, -1);
-    print("$ster\n");
-};
+// Opdracht 1 a t/m h
+//$ster="";
+//for ($a= 0; $a<=3; $a=$a+1){
+//    $extraster="*";
+//    $ster=$ster.$extraster;
+//    print ("$ster\n");
+//
+//}
+//for ($i= 0; $i <=3; $i=$i+1){
+//    $ster = substr($ster, 0, -1);
+//    print("$ster\n");
+//};
+// Opdracht 1 i t/m l
+//$str='';
+//for ($i=1; $i<=100; $i=$i+1) {
+//    if ($i % 2 == 0){
+//        $str=" ".$i;
+//        print ($str);
+//    }
+//}
+// Opdracht 1 m t/m p
+//$int = [];
+//for ($i=1; $i<=20; $i=$i+1){
+//    $int = rand(0,99);
+//    print ("$int ");
+//}
+//for ($i=1; $i<=8; $i=$i+1){
+//    for ($a=1; $a<=8; $a=$a+1){
+//        $breed = rand(0,99);
+//        print ("$breed ");
+//    }
+//    $leng = rand(0,99);
+//   print ("$leng\n");
+//}
+// Opdracht 2
